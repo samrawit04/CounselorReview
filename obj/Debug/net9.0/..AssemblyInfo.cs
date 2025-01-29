9bc5f12f0f61a9b196d3df2c3a95660af27f1b6f
samrawit04 <amaresamr93@gmail.com>
2025-01-29 16:20:58 +0400

    updates

--- obj/Debug/net9.0/..AssemblyInfo.cs
+++ obj/Debug/net9.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9244ff687b974f410be9efd5dca3acaef7f9d123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10572ca7a549c7b1d33bb65c79f9ae4659bfa9c5")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
